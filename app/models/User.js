57ec0145075e686ba7d10e1607e288530e06ce61
Luis Alberto Villegas Valenzuela <lualviva97@gmail.com>
2022-04-24 14:26:58 -0500

    Clase User con sus pruebas para la creación de objetos y getters

---
+++ app/models/User.js
@@ -0,0 +1,29 @@
+class User {
+  constructor(id, username, name, bio, dateCreated, lastUpdated) {
+    this.id = id;
+    this.username = username;
+    this.name = name;
+    this.bio = bio;
+    this.dateCreated = new Date();
+    this.lastUpdated = new Date();
+  }
+  get getUsername() {
+    return this.username;
+  }
+  get getBio() {
+    return this.bio;
+  }
+  get getDateCreated() {
+    return this.dateCreated;
+  }
+  get getLastUpdated() {
+    return this.lastUpdated;
+  }
+  //   set setUsername(username) {
+  //     this.username = username;
+  //   }
+  //   set setBio(bio) {
+  //     this.bio = bio;
+  //   }
+}
+module.exports = User;
---
+++ test/User.test.js
@@ -0,0 +1,19 @@
+const User = require("./../app/models/User");
+describe("Unit Tests for User class", () => {
+  test("Create an User object", () => {
+    const user = new User(1, "villegas97", "Luis", "Bio");
+    expect(user.id).toBe(1);
+    expect(user.username).toBe("villegas97");
+    expect(user.name).toBe("Luis");
+    expect(user.bio).toBe("Bio");
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
+  });
+  test("Add getters", () => {
+    const user = new User(1, "villegas97", "Luis", "Bio");
+    expect(user.getUsername).toBe("villegas97");
+    expect(user.getBio).toBe("Bio");
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.getLastUpdated).not.toBeUndefined();
+  });
+});
